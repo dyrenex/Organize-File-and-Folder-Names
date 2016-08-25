5df4d0f416a5cef45f11bf6fe82715955cbf8415
Derek Santos <derekhalo@gmail.com>
2016-08-25 00:03:28 -0400

    Less code, for more funcionality...
    I feel much happier showing this on github than the earlier mess I had
    before. It is also much less code for more funcionality.

--- Readme.txt
+++ Readme.txt
@@ -8,21 +8,63 @@ Python 3!
 What does this script do?
 =========================
 
-This script deletes any unwanted words, phrases, and hypens that are found within a file name in the current folder, and any folder within.
+This script deletes any unwanted words, phrases, and hyphens that are found within a file name in the current folder, and any folder within.
 
 Where does it work?
 ===================
 
-SHOULD only work on Mac and Linux. For now.
+Works on Mac and Linux.
+SHOULD work on Windows, but still not sure yet. 
 
 How does it work?
 =================
 
 WordsToDelete.txt is what words you would like to delete from a given file name. Put every word or phrase on separate lines please.
 
+
+Options!
+========
+
+To easily choose what is effected by the program, I have included an 'options' dictionary within the python script.
+Just change 'True' to 'False' to activate and deactivate an option.
+     'True'  = Active
+     'False' = Inactive
+
+List of what everything does:
+
+     'words_file':
+          Change 'NULL' to the file that contains words that you want to delete.
+          !!! Please make sure each word or phrase are on separate lines !!!
+
+     'include_files':
+          When this is True, files will be changed by the program.
+
+     'include_folders':
+          When this is True, folders will be changed by the program.
+
+     'periods':
+          When this is True, periods will be changed to a single space.
+
+     'hyphens':
+          When this is True, hyphens will be changed to a single space.
+
+     'trailing_spaces':
+          When this is True, trailing spaces in names will be deleted.
+          Example:
+               Before: "TempFile   .txt"
+               After: "TempFile.txt"
+
+     'double_spaces':
+          When this is True, double spaces will be replaced by a single space.
+
+
 Todo
 ====
-
-[ X ] Words and Phrases get delete in file names
-[ ] Get rid of trailing spaces after the names
+[ X ] Include folders independently
+[ X ] Include files independently
+[ X ] Periods get deleted in file and folder names
+[ X ] Hyphens get deleted in file and folder names
+[ X ] Get rid of trailing spaces after file and folder names
+[ X ] Replaces double spaces to single spaces
+[ X ] Words and Phrases get deleted in file and folder names
 [ ] If it finds a specific file extension not within a folder, in the current directory, it will make a folder with it's name, and move the file inside of the new folder.
--- organizefiles.py
+++ organizefiles.py
@@ -1,55 +1,109 @@
 import os
-from glob import glob
+#from glob import glob
 
-"""
-    If you have specific words to delete
-    create a file and paste the name of
-    the file below.
-        Ex:
-            WordsToDeleteFile = 'WordsToDelete.txt'
+options = {
+            'words_file'        : 'WordsToDelete.txt',
+            'include_files'     : True,
+            'include_folders'   : True,
+            'periods'           : True,
+            'hyphens'           : True,
+            'trailing_spaces'   : True,
+            'double_spaces'     : False
+          }
 
-    If there are no words, then it will not look for a file.
-    Please keep WordsToDeleteFile equal to ''
-"""
+def trailing_spaces(word: str):
+    extension_pos = word.find('.') #starting position of file extensions example: .py
 
-replace_options = {
-                    'words_file': 'NULL', # Name file with words in them
-                    'periods': True, # would you like to replace periods?
-                    'hyphens': True, # would you like to replace hyphens?
-                  }
+    while word[extension_pos - 1:extension_pos] == ' ':
+        word = word[:extension_pos - 1] + word [extension_pos:]
+        extension_pos = word.find('.')
 
-def change_name(file_name: str):
+    return word
 
-    global replace_options
 
-    if (replace_options['periods']):
-        file_name = file_name.replace('.', ' ', (file_name.count('.') - 1))
+def remove_words(word: str):
+    try:
+        open_file = open(options['words_file'], 'r')
 
-    if(replace_options['hyphens']):
-        file_name = file_name.replace('-', '')
+        for current in open_file:
+            current = current.replace('\n', '') # incase there are any endlines
+            word = word.replace(current, '')
 
-    if replace_options['words_file'] != 'NULL':
-        try:
-            words_file = open(replace_options['words_file'])
+        open_file.close()
 
-            for word in words_file:
-                file_name = file_name.replace(word, '')
+        return str(word)
 
-            words_file.close()
+    except FileNotFoundError:
+        print('File not given or found')
 
-        except FileNotFoundError:
-            print ('File not given or found')
+def removing_doubles(word: str):
+    while word.find('  ') != -1:
+        word = word.replace('  ', ' ')
+    return word
 
-    return file_name
+def change_name(word: str, is_file: bool):
 
+    global options
+
+    if options['periods'] and is_file == True:
+        word = word.replace('.', ' ', (word.count('.') - 1))
+
+    elif options['periods'] and is_file == False:
+        word = word.replace('.', ' ')
+
+    if options['hyphens']:
+        word = word.replace('-', ' ')
+
+    if options['words_file'] != 'NULL':
+        word = remove_words(word)
+
+    if options['double_spaces']:
+        word = removing_doubles(word)
+
+    if options['trailing_spaces']:
+        word = trailing_spaces(word)
+
+    return str(word)
 
 if __name__ == "__main__":
 
-    for (folders, subfolders, files) in os.walk('.'):
-        for each_file in files:
-            try:
-                os.rename ( folders + '/' + each_file, folders + '/' + change_name(each_file) )
-            except Exception as e:
-                print (e)
+    for x in range(2): # need to pass twice or some files won't get checked
+
+        for (folders, subfolders, files) in os.walk('.'):
+
+            if options['include_files']:
+                if folders[0:3] != '.\.' and folders[0:3] != './.':
+
+                    #Files
+                    for each_file in files:
+                        try:
+                            if each_file[0:1] != '.':  # ignore hidden files
+                                new_name = change_name(each_file, True)
+
+                                if new_name != each_file:
+                                    os.rename(folders + '/' + each_file,
+                                              folders + '/' + new_name)
+
+                        except OSError as oserror:
+                            print(oserror)
+                        except Exception as e:
+                            raise e
+
+                #Folders
+                if options['include_folders']:
+                    for each_folder in subfolders:
+
+                        try:
+                            if each_folder[0:1] != '.': #ignore hidden files
+                                new_name = change_name(each_folder, False)
+
+                                if new_name != each_folder:
+                                    os.rename ( folders + '/' + each_folder,
+                                                folders + '/' + new_name)
+
+                        except OSError as oserror:
+                            print(oserror)
+                        except Exception as e:
+                            raise e
 
     print ('done!')
